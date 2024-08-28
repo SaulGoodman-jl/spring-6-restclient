8427027a71e77ef56c0f25c44fddce84515a1945
SaulGoodman-jl <2246515235@qq.com>
2024-08-28 23:11:49 +0800

    Implement Create Beer Entity

---
+++ src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring6restclient.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Configuration
+public class RestTemplateBuilderConfig {
+
+    @Value("${rest.template.rootUrl}")
+    String rootUrl;
+
+    @Bean
+    OAuth2AuthorizedClientManager auth2AuthorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                               OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
+        OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        AuthorizedClientServiceOAuth2AuthorizedClientManager auth2AuthorizedClientManager = new AuthorizedClientServiceOAuth2AuthorizedClientManager
+                (clientRegistrationRepository, oAuth2AuthorizedClientService);
+
+        auth2AuthorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return auth2AuthorizedClientManager;
+    }
+
+    @Bean
+    public RestClient.Builder restClientBuilder(RestTemplateBuilder restTemplateBuilder) {
+        return RestClient.builder(restTemplateBuilder.build());
+    }
+
+    @Bean
+    RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer,
+                                            OAuthClientInterceptor interceptor) {
+        assert rootUrl != null;
+        return configurer.configure(new RestTemplateBuilder())
+                .additionalInterceptors(interceptor)
+                .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl));
+    }
+}
