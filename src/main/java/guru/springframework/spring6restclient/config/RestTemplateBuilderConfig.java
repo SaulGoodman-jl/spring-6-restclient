39ac88513d348231e28e81a9480486b6d9b3a959
SaulGoodman-jl <2246515235@qq.com>
2024-09-03 15:13:21 +0800

    Logbook with Spring RestTemplate

--- src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6restclient/config/RestTemplateBuilderConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration
 public class RestTemplateBuilderConfig {
@@ -39,8 +41,11 @@ public class RestTemplateBuilderConfig {
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer,
                                             OAuthClientInterceptor interceptor) {
         assert rootUrl != null;
+
+        LogbookClientHttpRequestInterceptor logbookClientHttpRequestInterceptor = new LogbookClientHttpRequestInterceptor(Logbook.builder().build());
+
         return configurer.configure(new RestTemplateBuilder())
-                .additionalInterceptors(interceptor)
+                .additionalInterceptors(interceptor, logbookClientHttpRequestInterceptor)
                 .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl));
     }
 }
